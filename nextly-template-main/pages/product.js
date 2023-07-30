4c222af563bd810119b63a2de0939bb39c1a6272
aliraza4343 <37452510+aliraza4343@users.noreply.github.com>
2023-07-30 23:53:22 +0500

    product card

--- nextly-template-main/pages/product.js
+++ nextly-template-main/pages/product.js
@@ -13,19 +13,41 @@ const Product = () => {
             id: 1,
             name: "QUANTFARMING ULTIMATE TRADING INDICATOR"
         },
-        price: 795,
-        formattedPrice: "$795"
+        price: 499,
+        formattedPrice: "$499",
+        description: "Saves you time and effort by automating your trading strategy, allowing you to focus on other aspects of your trading approach"
     };
     const productInfo2 = {
         image:
             "https://images.squarespace-cdn.com/content/v1/644b3fec2f86f819f40064b7/1686147656602-TBLPVN9YQ3P6BFBXIXTO/Screenshot+2023-06-07+071932.png?format=750w",
         brand: {
             id: 2,
-            name: "MT4 RISK MANAGEMENT"
+            name: "Momemtum (EMA Crossover)"
         },
-        price: 795,
-        formattedPrice: "$795"
+        price: 799,
+        formattedPrice: "$799"
     };
+    const productInfo3 = {
+        image:
+            "https://images.squarespace-cdn.com/content/v1/644b3fec2f86f819f40064b7/1686147656602-TBLPVN9YQ3P6BFBXIXTO/Screenshot+2023-06-07+071932.png?format=750w",
+        brand: {
+            id: 2,
+            name: "Mean Reversion (Rsi Cross below and above)"
+        },
+        price: 1499,
+        formattedPrice: "$1499"
+    };
+    const productInfo4 = {
+        image:
+            "https://images.squarespace-cdn.com/content/v1/644b3fec2f86f819f40064b7/1686147656602-TBLPVN9YQ3P6BFBXIXTO/Screenshot+2023-06-07+071932.png?format=750w",
+        brand: {
+            id: 2,
+            name: "Risk Management"
+        },
+        price: 1299,
+        formattedPrice: "$1299"
+    };
+    
     return (
         <>
             <Head>
@@ -39,12 +61,12 @@ const Product = () => {
 
             <Navbar />
             <Container>
-                <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
+                <h1 className="z-20 relative text-4xl font-bold leading-snug tracking-tight  lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight text-white">
                     Our Products
                 </h1>
             </Container>
             <Container>
-            <div className = "flex flex-col md:flex-row"
+                <div className="flex flex-col"
                 // style={{
                 //     display: 'flex',
                 //     flexDirection: ['column', 'row'],
@@ -52,13 +74,41 @@ const Product = () => {
                 //     marginLeft: '5rem',
                 //     marginRight: '5rem',
                 // }}
-            >
-                <div className = "flex-1 shadow-2xl m-4 p-2 rounded-lg dark:bg-trueGray-800"><RecipeReviewCard  {...productInfo} /></div>
-                <div className = "flex-1 shadow-2xl m-4 p-2 rounded-lg dark:bg-trueGray-800"><RecipeReviewCard  {...productInfo2} /></div>
-                <div className = "flex-1 shadow-2xl m-4 p-2 rounded-lg dark:bg-trueGray-800"><RecipeReviewCard  {...productInfo2} /></div>
-            </div>
+                >
+                    <div className="flex-1" ><RecipeReviewCard  {...productInfo} /></div>
+                    <div className="flex-1"><RecipeReviewCard  {...productInfo2} /></div>
+                    <div className="flex-1"><RecipeReviewCard  {...productInfo3} /></div>
+                    <div className="flex-1"><RecipeReviewCard  {...productInfo4} /></div>
+                </div>
+                <div className="flex justify-center items-center">
+                    <div className="w-80 bg-trueGray-100 dark:bg-trueGray-800 shadow-lg rounded-lg p-6">
+                        <h2 className="text-2xl font-semibold mb-4">Monthly Subscription</h2>
+                        <ul className="space-y-2">
+                            <li className="flex items-center">
+                                <svg className="w-5 h-5 text-green-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                                </svg>
+                                <span>Feature 1</span>
+                            </li>
+                            <li className="flex items-center">
+                                <svg className="w-5 h-5 text-green-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                                </svg>
+                                <span>Feature 2</span>
+                            </li>
+                            <li className="flex items-center">
+                                <svg className="w-5 h-5 text-green-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                                </svg>
+                                <span>Feature 3</span>
+                            </li>
+                        </ul>
+                        <h2 className="text-4xl font-semibold m-4 text-center text-green-500">$90</h2>
+                        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-3xl w-full">Subscribe</button>
+                    </div>
+                </div>
             </Container>
-           
+
 
             <Footer />
             {/* <PopupWidget /> */}
